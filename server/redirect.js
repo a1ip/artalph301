5475a9c19efbaebb566d9496b340d4713f817b4f
Phil Rigovanov <phil@rigovanov.ru>
2014-03-24 20:02:06 +0400

    smart redirect

--- server/redirect.js
+++ server/redirect.js
@@ -1,9 +1,9 @@
-REDIRECT_URL = 'http://www.artalph.com/';
+REDIRECT_URL = 'http://www.artalph.com';
 
 WebApp.connectHandlers
   .use(function(req, res, next) {
     console.log(req.url);
-    var location = REDIRECT_URL;
+    var location = REDIRECT_URL + req.url;
     res.writeHead(301, {'Location': location});
     res.end();
   });
