6f6d50266836dd3a154f81932f21e91cd97bdacb
Phil Rigovanov <phil@rigovanov.ru>
2014-03-24 19:19:48 +0400

    first commit

---
+++ server/redirect.js
@@ -0,0 +1,10 @@
+REDIRECT_URL = '
+http://www.artalph.com/';
+
+WebApp.connectHandlers
+  .use(function(req, res, next) {
+    console.log(req.url);
+    var location = REDIRECT_URL;
+    res.writeHead(301, {'Location': location});
+    res.end();
+  });
